dcfeb4bf9ee33602fb8c293549f2f980afc2315b
edhn <MaelOrEli@gmail.com>
2014-11-06 03:14:35 +1300

    Finished Dead Logs
    Completed the death logs.

--- lua/autorun/logs_client.lua
+++ lua/autorun/logs_client.lua
@@ -1,60 +1,84 @@
-if SERVER then return end
-print("Logs Client Loaded")
+if SERVER then return end --Make sure the server does not try to run any lua in this file.
 
-
-death_logs = {}
+death_logs = {} --List that will hold all the information when player dies
 
 function player_die_info( data )
 
-	victim = data:ReadString()
-	killer = data:ReadString()
-	weapon = data:ReadString()
-	timestamp = data:ReadLong()
+	victim = data:ReadString() ------------------------
+	killer = data:ReadString() -----Reading information From the user
+	weapon = data:ReadString() -----Message sent from the server
+	timestamp = data:ReadLong() -----------------------
+
 	table.insert(death_logs, {victim = victim, killer = killer, weapon = weapon, timestamp = timestamp})
+	--[[
+		The above line inserts a table containing all the information from the players death that we will
+		need to display in our list view into the death_logs table.
+		
+		It will look something like this:
+
+		death_logs{
+			1 	{
+					victim = "Bob",
+					killer = "Jim",
+					weapon = "gun",
+					timestamp = *timestamp*
+				},
+
+			2 	{
+					victim = "Jim"
+					killer = "Bob"
+					weapon = "gun"
+					timestamp = *timestamp*
+				}
+			
+		}
+
+	]]
+
 end
 usermessage.Hook( "player_die_info", player_die_info );
 
 
---[[
-
-
-
-]]
-
+--This function is used to calculate the time that has passed since
+--the event has happened.
+--It takes the timestamp we saved to our deaths_logs as a parameter
+--and returns a value either "x Secs", "x Mins", or "x Hours"
 function time_ago( timestamp )
-		timeago = (os.time() - timestamp) / 2
+		timeago = os.time() - timestamp
 		if timeago < 60 then
 			timeago = timeago .. " Secs"
 		elseif timeago > 3600 then
 			timeago = (timeago / 60) .. " Hours"
 		else
-			timeago = timeago .. " Mins"
+			timeago = (timeago / 60) .. " Mins"
 		end
 		return timeago
 end
 
+--Create the derma menu.
 function menu()
-	if LocalPlayer():IsAdmin() then
+	if LocalPlayer():IsAdmin() then --Only admins can open the logs.
 
+		--Create the frame
 		local frame = vgui.Create("DFrame")
 		frame:SetSize(400, 400)
 		frame:MakePopup()
 
+		--Create the list to display the logs.
 		local loglist = vgui.Create("DListView", frame)
-		loglist:SetSize(frame:GetWide() - 10, frame:GetTall() - 30)
+		loglist:SetSize(frame:GetWide() - 10, frame:GetTall() - 30) --So you do not need to change values if you decide to change frame size.
 		loglist:SetPos(5, 25)
-		loglist:AddColumn( "Time" )
+		loglist:AddColumn( "Time" ):SetFixedWidth(60) --Just so the time column does not take up so much space.
 		loglist:AddColumn( "Action" )
-		loglist:ColumnWidth(102)
 
 		for k, v in pairs(death_logs) do
 			message = v['victim'] .. " was killed by " .. v['killer'] .. " with " .. v['weapon']
-			loglist:AddLine(time_ago(v['timestamp']), message)
+			loglist:AddLine(time_ago(v['timestamp']), message) --Add an entry to the list view containing the time and message.
 		end
 
 
 	else
-		print("Insufficient permissions.")
+		print("Insufficient permissions.") --This will print if the user is not an admin.
 	end
 
 	
--- lua/autorun/logs_server.lua
+++ lua/autorun/logs_server.lua
@@ -1,22 +1,19 @@
-if CLIENT then return end
-print("Logs Server Loaded")
+if CLIENT then return end --Make sure the client does not try to run any lua in this file.
+
 -- Function to call when a player dies.
-
---new timestamp - old timestamp = time in seconds past since event
-
 function player_die( victim, inflictor, killer)
 
-	player_name = victim:Name()
-	weapon = inflictor:GetClass()
-	killer_name = killer:Name()
-	time = os.time()
+	player_name = victim:Name() --Get the Vicims name
+	weapon = inflictor:GetClass() --Get the weapon the victim was killed with
+	killer_name = killer:Name() --Get killers name
+	time = os.time() --Get the time the player died
 
-	umsg.Start( "player_die_info" );
-	umsg.String(player_name)
-	umsg.String(killer_name)
-	umsg.String(weapon)
-	umsg.Long(time)
-	umsg.End();
+	umsg.Start( "player_die_info" );-------------------------
+		umsg.String(player_name)-----------------------------
+		umsg.String(killer_name)--------Sending the Data to 
+		umsg.String(weapon)-------------be recieved by client
+		umsg.Long(time)--------------------------------------
+	umsg.End();----------------------------------------------
 
 
 end
