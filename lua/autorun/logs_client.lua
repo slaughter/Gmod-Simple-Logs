b47b13dfe6301bfbad81e33274e1ff8d27870dc1
edhn <MaelOrEli@gmail.com>
2014-11-06 16:28:00 +1300

    Updates
    -Added settings tab.
    -You can now double click on an entry to print it to console.
    -Logs now display the attackers steamID.

--- lua/autorun/logs_client.lua
+++ lua/autorun/logs_client.lua
@@ -8,6 +8,11 @@
 
 if SERVER then return end --Make sure the server does not try to run any lua in this file.
 
+--SETTING UP CONVARS
+CreateClientConVar("fgh_logs_print_to_console", 0, true, false)
+
+
+
 --Add tables Here--
 death_logs = {} --List that will hold all the information when player dies
 damage_logs = {} --List that will hold all the information when player gets hurt
@@ -22,10 +27,11 @@ function player_die_info( data )
 
 	victim = data:ReadString() ------------------------
 	killer = data:ReadString() -----Reading information From the user
-	weapon = data:ReadString() -----Message sent from the server
+	killerID = data:ReadString() -----message sent from the server
+	weapon = data:ReadString() ------------------------
 	timestamp = data:ReadLong() -----------------------
 
-	table.insert(death_logs, {victim = victim, killer = killer, weapon = weapon, timestamp = timestamp})
+	table.insert(death_logs, {victim = victim, killer = killer, killerID = killerID, weapon = weapon, timestamp = timestamp})
 	--[[
 		The above line inserts a table containing all the information from the players death that we will
 		need to display in our list view into the death_logs table.
@@ -57,24 +63,31 @@ usermessage.Hook( "player_die_info", player_die_info );
 function player_hurt_info( data )
 
 	victim = data:ReadString() ------------------------
-	attacker = data:ReadString() --Reading information From the user
-	damage = data:ReadLong() ------Message sent from the server
+	attacker = data:ReadString() ----Reading information From the user
+	attackerID = data:ReadString() --
+	damage = data:ReadLong() --------message sent from the server
 	timestamp = data:ReadLong() -----------------------
 
 	--See line 24
-	table.insert(damage_logs, {victim = victim, attacker = attacker, damage = damage, timestamp = timestamp})
+	table.insert(damage_logs, {victim = victim, attacker = attacker, attackerID = attackerID, damage = damage, timestamp = timestamp})
 
 end
 usermessage.Hook( "player_hurt_info", player_hurt_info );
 
 function player_spawn_info( data )
 
-	player_name = data:ReadString() ------------------------
-	prop = data:ReadString() --Reading information From the user
-	timestamp = data:ReadLong() ------Message sent from the server
+	player_name = data:ReadString() -----------------------------
+	playerID = data:ReadString() -----Reading information From the user
+	prop = data:ReadString() ---------message sent from the server
+	timestamp = data:ReadLong() ------------------------------------
+	
+	message = player_name .. "(" .. playerID .. ") spawned " .. prop
+	if GetConVar("fgh_logs_print_to_console"):GetInt() == 1 then
+		print(message)
+	end
 
 	--See line 24
-	table.insert(prop_logs, {player_name = player_name, prop = prop, timestamp = timestamp})
+	table.insert(prop_logs, {player_name = player_name, playerID = playerID, prop = prop, timestamp = timestamp})
 
 end
 usermessage.Hook( "player_spawn_info", player_spawn_info );
@@ -127,25 +140,48 @@ function menu()
 
 		--Create the frame
 		local frame = vgui.Create("DFrame")
-		frame:SetSize(400, 400)
+		frame:SetSize(600, 400)
 		frame:SetTitle("Simple Logs | fghdx.me")
 		frame:MakePopup()
 		
 		local tabs = vgui.Create( "DPropertySheet", frame )
 		tabs:SetPos( 5, 30 )
 		tabs:SetSize( frame:GetWide() - 10, frame:GetTall() - 35 )
-	
+		
+		--Settings Tab. The Panel to add elements to.
+		local settings_panel = vgui.Create("DPanel")
+		settings_panel:SetSize(tabs:GetWide() - 10, tabs:GetTall() - 50)
+		settings_panel:SetPos(5, 50)
+		settings_panel:SetBackgroundColor(Color(0,0,0,0))
+
+		--Add checkbox the the Settings Panel.
+		local ptc_check = vgui.Create("DCheckBoxLabel", settings_panel)
+		ptc_check:SetPos(5, 5)
+		ptc_check:SetConVar("fgh_logs_print_to_console")
+		ptc_check:SetValue(GetConVar("fgh_logs_print_to_console"):GetInt())
+		ptc_check:SetText("Print logs to console")
+		ptc_check:SizeToContents()
+
 		--Create the list to display the logs.
 		local loglist_deaths = vgui.Create("DListView")
 		loglist_deaths:SetSize(tabs:GetWide() - 10, tabs:GetTall() - 50) --So you do not need to change values if you decide to change frame size.
 		loglist_deaths:SetPos(5, 50)
 		loglist_deaths:AddColumn( "Time" ):SetFixedWidth(60) --Just so the time column does not take up so much space.
 		loglist_deaths:AddColumn( "Action" )
+		loglist_deaths.DoDoubleClick = function(parent, index, list)
+					print("----Selected Entry ----")
+					print(list:GetValue(2))
+					print("-----------------------")
+				end
 
 		for i = #death_logs, 1, -1 do --Iterate through the list death_logs in reverse so most recent items are at the top.
 		  v = death_logs[i]
-			message = v['victim'] .. " was killed by " .. v['killer'] .. " with " .. v['weapon']
+			message = v['victim'] .. " was killed by " .. v['killer'] .. "(" .. v['killerID'] ..  ") with " .. v['weapon']
+			if GetConVar("fgh_logs_print_to_console"):GetInt() == 1 then
+				print(time_ago(v['timestamp']), message)
+			end
 			loglist_deaths:AddLine(time_ago(v['timestamp']), message) --Add an entry to the list view containing the time and message.
+		
 		end
 
 		--Create the list to display damage logs
@@ -154,10 +190,18 @@ function menu()
 		loglist_damage:SetPos(5, 50)
 		loglist_damage:AddColumn( "Time" ):SetFixedWidth(60) --Just so the time column does not take up so much space.
 		loglist_damage:AddColumn( "Action" )
+		loglist_damage.DoDoubleClick = function(parent, index, list)
+					print("----Selected Entry ----")
+					print(list:GetValue(2))
+					print("-----------------------")
+				end
 
 		for i = #damage_logs, 1, -1 do --Iterate through the list damage_logs in reverse so most recent items are at the top.
 		  v = damage_logs[i]
-			message = v['victim'] .. " was hurt by " .. v['attacker'] .. " for " .. v['damage']
+			message = v['victim'] .. " was hurt by " .. v['attacker'] .. "(" .. v['attackerID'] .. ") for " .. v['damage']
+			if GetConVar("fgh_logs_print_to_console"):GetInt() == 1 then
+				print(time_ago(v['timestamp']), message)
+			end
 			loglist_damage:AddLine(time_ago(v['timestamp']), message) --Add an entry to the list view containing the time and message.
 		end
 		
@@ -167,13 +211,17 @@ function menu()
 		loglist_props:SetPos(5, 50)
 		loglist_props:AddColumn( "Time" ):SetFixedWidth(60) --Just so the time column does not take up so much space.
 		loglist_props:AddColumn( "Action" )
+		loglist_props.DoDoubleClick = function(parent, index, list)
+					print("----Selected Entry ----")
+					print(list:GetValue(2))
+					print("-----------------------")
+				end
 
 		for i = #prop_logs, 1, -1 do --Iterate through the list prop_logs in reverse so most recent items are at the top.
 		  v = prop_logs[i]
-			message = v['player_name'] .. " spawned " .. v['prop']
+			message = v['player_name'] .. "(" .. playerID  .. ") spawned " .. v['prop']
 			loglist_props:AddLine(time_ago(v['timestamp']), message) --Add an entry to the list view containing the time and message.
 		end
-		
 		------------------------------------------------------
 		------------------ADD NEW LISTS HERE------------------
 		------------------------------------------------------
@@ -203,6 +251,10 @@ function menu()
 		tabs:AddSheet( "Deaths", loglist_deaths, false, false, "Death Logs" )
 		tabs:AddSheet( "Damage", loglist_damage, false, false, "Damage Logs" )
 		tabs:AddSheet( "Props", loglist_props, false, false, "Prop Logs" )
+		
+
+		--Keep this one last.
+		tabs:AddSheet( "Settings", settings_panel, false, false, "Settings" )
 		
 		--[[
 			EXAMPLE:
--- lua/autorun/logs_server.lua
+++ lua/autorun/logs_server.lua
@@ -16,18 +16,21 @@ function player_die( victim, inflictor, killer)
 	--The workaround is setting weapon equal to the weapon the killer is holding
 	--As far as I could figure out.
 
-	player_name = victim:Name() --Get the Vicims name
-	weapon = killer:GetActiveWeapon():GetClass() --Get the weapon the victim was killed with
-	killer_name = killer:Name() --Get killers name
-	time = os.time() --Get the time the player died
+	if killer:IsPlayer() then --Make sure he was killed by another player.
+		player_name = victim:Name() --Get the Vicims name
+		weapon = killer:GetActiveWeapon():GetClass() --Get the weapon the victim was killed with
+		killer_name = killer:Name() --Get killers name
+		killerID = killer:SteamID() --Get killers ID
+		time = os.time() --Get the time the player died
 
-	umsg.Start( "player_die_info" );-------------------------
-		umsg.String(player_name)-----------------------------
-		umsg.String(killer_name)--------Sending the Data to 
-		umsg.String(weapon)-------------be recieved by client
-		umsg.Long(time)--------------------------------------
-	umsg.End();----------------------------------------------
-
+		umsg.Start( "player_die_info" );-------------------------
+			umsg.String(player_name)-----------------------------
+			umsg.String(killer_name)--------Sending the Data to 
+			umsg.String(killerID)----------- 
+			umsg.String(weapon)-------------be recieved by client
+			umsg.Long(time)--------------------------------------
+		umsg.End();----------------------------------------------
+	end
 
 end
 hook.Add( "PlayerDeath", "playerDeath", player_die )
@@ -36,13 +39,15 @@ hook.Add( "PlayerDeath", "playerDeath", player_die )
 function player_take_damage( victim, attacker, healthLeft, damageTaken )
 	if attacker:IsPlayer() then --Make sure player is hurt by another player, rather than the world or console.
 		player_name = victim:Name() --Get the Vicims name
-		attacker = attacker:Name() --Get attackers name
+		attacker_name = attacker:Name() --Get attackers name
+		attackerID = attacker:SteamID() --Get attackers id
 		damage = damageTaken --Get the damage Dealt
 		time = os.time() --Get the time of the event
 
 		umsg.Start( "player_hurt_info" );------------------------
 			umsg.String(player_name)-----------------------------
-			umsg.String(attacker)-----------Sending the Data to 
+			umsg.String(attacker_name)-----------Sending the Data to 
+			umsg.String(attackerID)---------
 			umsg.Long(damage)---------------be recieved by client
 			umsg.Long(time)--------------------------------------
 		umsg.End();----------------------------------------------
@@ -54,10 +59,12 @@ hook.Add( "PlayerHurt", "playerHurt", player_take_damage )
 function player_spawn_prop( ply, model )
 
 	player_name = ply:Name()
+	playerID = ply:SteamID()
 	time = os.time() --Get the time of the event
 
 	umsg.Start( "player_spawn_info" );------------------------
 		umsg.String(player_name)-----------------------------
+		umsg.String(playerID)-----------------------------
 		umsg.String(model)------------Sending the Data to 
 		umsg.Long(time)---------------be recieved by client
 	umsg.End();----------------------------------------------
